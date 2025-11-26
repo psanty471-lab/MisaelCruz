9f197ac408c874b488fd850b6eebc55b6d48f78f
psanty471-lab <psanty471@gmail.com>
2025-11-26 12:42:23 -0500

    ssd

---
+++ JavaScrip/primerejemplo.js
@@ -0,0 +1,26 @@
+console.log("Hola Humanos");
+alert("Hola Mundo, esto es una invación") 
+
+function frio(){
+    document.getElementById('carita').src='images/frio.png';
+    alert("Po que tines frio. es lo mejor");
+}
+function calor(){
+    document.getElementById('carita').src='images/calor.png';
+}
+function cambiarColor(){
+    document.getElementById('menu').style.color='#75746bff';
+
+}
+function cambiarLogo(){
+    document.getElementById('logo').src='images/logo2.jpg';
+}
+function cambiarFondo(){
+     document.getElementById('menu').style.background='#d6d4c6ff';
+}
+function cambiodeTexto(){
+    document.getElementById('Subtitulo').textContent="tengan paciencia, aun no se usar esta cosa";
+}
+function fondoAside(){
+    document.getElementById('aside').style.background='#506b72ff';
+}
