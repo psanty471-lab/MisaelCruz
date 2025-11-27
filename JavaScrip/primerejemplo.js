4d5e682f17257e379d991b16497345757ca9e751
psanty471-lab <psanty471@gmail.com>
2025-11-27 14:02:06 -0500

    sd

--- JavaScrip/primerejemplo.js
+++ JavaScrip/primerejemplo.js
@@ -2,11 +2,11 @@ console.log("Hola Humanos");
 alert("Hola Mundo, esto es una invación") 
 
 function frio(){
-    document.getElementById('carita').src='images/frio.png';
+    document.getElementById('temperatura').src='images/frio.png';
     alert("Po que tines frio. es lo mejor");
 }
 function calor(){
-    document.getElementById('carita').src='images/calor.png';
+    document.getElementById('temperatura').src='images/calor.png';
 }
 function cambiarColor(){
     document.getElementById('menu').style.color='#75746bff';
@@ -19,8 +19,20 @@ function cambiarFondo(){
      document.getElementById('menu').style.background='#d6d4c6ff';
 }
 function cambiodeTexto(){
-    document.getElementById('Subtitulo').textContent="tengan paciencia, aun no se usar esta cosa";
+    document.getElementById('Subtitulo').textContent="tengan paciencia, aun no se usar esta cosa"
+}
+function msj1(){
+   alert ("El desarrollo de un programa se basa en una idea");
+}
+function msj2(){
+   alert ("Un algoritmo se usa para resolver un problemas");
+}
+function msj3(){
+   alert ("Escogi bien mi carrera?");
 }
 function fondoAside(){
     document.getElementById('aside').style.background='#506b72ff';
 }
+function nuevotitulo(){
+    document.querySelector('title').textContent = "Tratando de usar Java Scrip";
+    alert("titulo cambiado correctamente")
